3b258af187d3c0d1f462c2cf454b9e5d51d7d95c
scarabus <dave.tong@incapturetechnologies.com>
2016-04-27 12:58:53 -0700

    Sigh

--- Libs/test.sh
+++ Libs/test.sh
@@ -3,9 +3,10 @@
 {
   ./gradlew test
 } || {
-  zip -r - `find .. -name reports` | uuencode - 2>/dev/null
-  cat RaptureCore/build/reports/tests/index.html &&\
-  cat RaptureCore/build/reports/tests/classes/rapture.dp.NestedSplitStepTest.html &&\
-  cat RaptureCore/build/reports/tests/classes/rapture.dp.SimpleForkStepTest.html &&\
-  cat RaptureCore/build/reports/tests/classes/rapture.dp.MemoryIndexHandlerTest.html
+  (echo Directory is `pwd`;
+  find .. -name reports;
+  zip -r - `find .. -name reports` | uuencode - 2>/dev/null;
+  echo RaptureCore/build/reports/tests/index.html */build/reports/tests/classes/*html;
+  cat RaptureCore/build/reports/tests/index.html */build/reports/tests/classes/*html;
+  echo "======";)
 }
