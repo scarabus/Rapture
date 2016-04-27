ef8681c5dbbee94d9b58027e1fdb343888eed605
scarabus <dave.tong@incapturetechnologies.com>
2016-04-27 12:44:13 -0700

    Sigh

--- Libs/test.sh
+++ Libs/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 {
-  ./gradlew test ;
-  zip -r - `find .. -name reports` | uuencode - 2>/dev/null
+  ./gradlew test
 } || {
+  zip -r - `find .. -name reports` | uuencode - 2>/dev/null
   cat RaptureCore/build/reports/tests/index.html &&\
   cat RaptureCore/build/reports/tests/classes/rapture.dp.NestedSplitStepTest.html &&\
   cat RaptureCore/build/reports/tests/classes/rapture.dp.SimpleForkStepTest.html &&\
